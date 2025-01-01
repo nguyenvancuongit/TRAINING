0d401ed9699c512471d94a32c2b6132eebee220c
nguyenvancuongit <nguyenvancuongit@gmail.com>
2025-01-01 14:58:17 +0700

    - Array

--- REACT_NATIVE/ReactNativeFrom0/MyReactNativeApp/App.tsx
+++ REACT_NATIVE/ReactNativeFrom0/MyReactNativeApp/App.tsx
@@ -1,34 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
-  const [name, setName] = useState<string>("");
-  const [age, setAge] = useState<number>(0);
+  const [students, setStudents] = useState([
+    { name: 'John', key: '1' },
+    { name: 'Doe', key: '2' },
+    { name: 'Jane', key: '3' },
+    { name: 'Doe', key: '4' },
+    { name: 'Jane', key: '5' },
+    { name: 'Doe', key: '6' },
+    { name: 'Jane', key: '7' },
+    { name: 'Doe', key: '8' },
+    { name: 'Jane', key: '9' },
+    { name: 'Doe', key: '10' },
+    { name: 'Jane', key: '11' },
+    { name: 'Doe', key: '12' },
+    { name: 'Jane', key: '13' },
+    { name: 'Doe', key: '14' },
+    { name: 'Jane', key: '15' },
+    { name: 'Doe', key: '16' },
+    { name: 'Jane', key: '17' },
+    { name: 'Doe', key: '18' },
+    { name: 'Jane', key: '19' },
+    { name: 'Doe', key: '20' },
+    { name: 'Jane', key: '21' },
+    { name: 'Doe', key: '22' },
+    { name: 'Jane', key: '23' },
+    { name: 'Doe', key: '24' },
+    { name: 'Jane', key: '25' },
+    { name: 'Doe', key: '26' },
+    { name: 'Jane', key: '27' },
+    { name: 'Doe', key: '28' },
+    { name: 'Jane', key: '29' },
+    { name: 'Doe', key: '30' },
+    { name: 'Jane', key: '31' },
+    { name: 'Doe', key: '32' },
+    { name: 'Jane', key: '33' },
+    { name: 'Doe', key: '34' },
+    { name: 'Jane', key: '35' },
+    { name: 'Doe', key: '36' },
+    { name: 'Jane', key: '37' },
+  ]);
 
   return (
     <View style={styles.container}>
       
-      <View >
-        <Text style={{ fontSize: 40, fontWeight: "600" }}>Name: {name}</Text>
+      <Text style={{
+        marginTop: 50,
+      }}>Students:</Text>
 
-        <TextInput
-          multiline
-          style={styles.text_input}
-          onChangeText={setName}
-        />
-      </View>
-      
-      <View>
-        <Text style={{ fontSize: 40, fontWeight: "600" }}>Age: {age}</Text>
-
-        <TextInput
-          keyboardType="numeric"
-          style={styles.text_input}
-          onChangeText={text => setAge(parseInt(text))}
-        />
-      </View>
+      <ScrollView>
+        {students.map(item => {
+          return (
+            <View key={item.key} style={{
+              padding: 30,
+              backgroundColor: 'pink',
+              marginBottom: 10
+            }}>
+              <Text>{item.name}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
 
     </View>
   );
@@ -38,13 +73,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  text_input: {
-    width: 200,
-    borderColor: 'gray',
-    borderWidth: 1,
-    padding: 15
-  }
 });
